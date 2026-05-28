ab9b44f8fb5d7c7fdfd3dc5dab22df9bd928cff0
tahsin-roza <tahsinroza10@gmail.com>
2026-05-28 13:25:49 +0600

    added navbar

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,17 +1,9 @@
-"use client";
-
-import { UserButton } from "@clerk/nextjs";
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
-
-export default function Home() {
-  const { data, isLoading } = useGetAccounts();
-
-  if (isLoading) return <div>Loading...</div>;
-
+const Home = () => {
   return (
     <div>
-      <UserButton afterSignOutUrl="/" />
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      Dashboard Page
     </div>
   );
-}
+};
+
+export default Home;
